36e0ad092c6e6115258cad177f4f0bb1ce3baa8f
Point Hua <huawenhaohhh@126.com>
2024-08-20 14:55:40 +0800

    update client

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include <ESP8266WiFi.h>
+
+const char *ssid = "esp_01swifi";
+const char *password = "12345678";
+
+IPAddress serverIP(192, 168, 4, 1); // AP模式ESP8266的IP地址
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("连接到AP...");
+  }
+  Serial.println("已连接到AP");
+  Serial.print("IP地址: ");
+  Serial.println(WiFi.localIP()); // 打印STA的IP地址
+
+  // 连接到服务器
+  WiFiClient client;
+  client.connect(serverIP, 5000);
+  if (client.connected()) {
+    Serial.println("已连接到服务器");
+    client.println("Hello from STA!");
+  }
+}
+
+void loop() {
+  // 这里可以添加代码来定期发送消息到服务器
+  delay(1000);
+}
