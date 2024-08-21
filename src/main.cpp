3eb537a623c03032024006dbc5c13ca645ea11fb
Point Hua <huawenhaohhh@126.com>
2024-08-21 17:13:56 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,23 @@ const char *ssid = "esp_01swifi";
 IPAddress serverIP(192, 168, 4, 1); // AP模式ESP8266的IP地址
 WiFiClient client;
 
+bool IsButtonPressed(uint8_t pin)
+{
+  if (digitalRead(pin) == HIGH) // button is pressed
+  {
+    delay(50);
+    if (digitalRead(pin) == HIGH) // button is pressed
+    {
+      return true;
+    }
+  }
+  else
+  {
+    return false;
+  }
+  return false;
+}
+
 void setup()
 {
   Serial.begin(115200);
@@ -21,21 +38,35 @@ void setup()
 
   // 连接到服务器
   client.connect(serverIP, 5000);
+
+  pinMode(0,INPUT);
 }
 
-String statu = "ON\r";
+// String statu = "OFF\r";
 
 void loop()
 {
   if (client.connected())
   {
     Serial.println("已连接到服务器");
-    Serial.println(statu);
-    client.print(statu);
-    statu == "ON\r" ? statu = "OFF\r" : statu = "ON\r";//要加/r否则server读取会出问题
+    // Serial.println(statu);
+    // client.print(statu);
+    // statu == "ON\r" ? statu = "OFF\r" : statu = "ON\r"; // 要加/r否则server读取会出问题
+
+    if (IsButtonPressed(0))
+    {
+      Serial.println("ON");
+      client.print("ON");
+    }
+    else
+    {
+      Serial.println("OFF");
+      client.print("OFF");
+    }
   }
   else
   {
+    Serial.println("连接断开！");
     client.connect(serverIP, 5000);
   }
   delay(1000);
